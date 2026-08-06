8451424ca6e966779ef6e0290b69c4953b70fc8a
Chamuditha Sanka <chamudithasanka98@gmail.com>
2026-08-06 17:19:38 +0530

    Configure Firebase initialization

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,3 +1,20 @@
-// Firebase configuration placeholder
-export const firebaseConfig = {};
-export default firebaseConfig;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBP4IndWIqgU8mIkyFWVl-Z0SmMzWW--ow",
+    authDomain: "sharebite-e1b2c.firebaseapp.com",
+    projectId: "sharebite-e1b2c",
+    storageBucket: "sharebite-e1b2c.firebasestorage.app",
+    messagingSenderId: "581786788459",
+    appId: "1:581786788459:web:1b7ef76f1da544990e072e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
