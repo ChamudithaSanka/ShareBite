f51a7ff5f5b730839886ed71319ae005fdad02dc
Adeepa Wedage <IT23826304@my.sliit.lk>
2026-08-20 09:34:18 +0530

    Update volunteer navigation and home

--- src/navigation/VolunteerNavigator.js
+++ src/navigation/VolunteerNavigator.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Text } from 'react-native';
 
 import VolunteerHomeScreen from '../screens/volunteer/VolunteerHomeScreen';
 import AvailableDeliveriesScreen from '../screens/volunteer/AvailableDeliveriesScreen';
 import ActiveDeliveryScreen from '../screens/volunteer/ActiveDeliveryScreen';
+import DeliveryDetailScreen from '../screens/volunteer/DeliveryDetailScreen';
 import ProfileScreen from '../screens/shared/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
+const JobsStack = createStackNavigator();
 const GREEN = '#1A7A4A';
+
+function JobsStackNav() {
+  return (
+    <JobsStack.Navigator screenOptions={{ headerShown: false }}>
+      <JobsStack.Screen name="AvailableDeliveries" component={AvailableDeliveriesScreen} />
+      <JobsStack.Screen name="DeliveryDetail" component={DeliveryDetailScreen} />
+    </JobsStack.Navigator>
+  );
+}
 
 const TabIcon = ({ routeName }) => (
   <Text style={{ fontSize: 20 }}>
@@ -39,7 +51,7 @@ export default function VolunteerNavigator() {
       })}
     >
       <Tab.Screen name="Home" component={VolunteerHomeScreen} />
-      <Tab.Screen name="Jobs" component={AvailableDeliveriesScreen} />
+      <Tab.Screen name="Jobs" component={JobsStackNav} />
       <Tab.Screen name="Active" component={ActiveDeliveryScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
